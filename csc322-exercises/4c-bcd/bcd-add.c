625aeb4ca79af8af4a2e629a0f251ad4575f5b7f
Burt Rosenberg <burt@cs.miami.edu>
2023-08-16 17:34:22 -0400

    ok

--- csc322-exercises/4c-bcd/bcd-add.c
+++ csc322-exercises/4c-bcd/bcd-add.c
@@ -16,6 +16,10 @@
 #define BUF_SIZE 1024
 #define DEBUG 0
 
+#ifndef IS_SUB
+#define IS_SUB 0
+#endif
+
 int ato_bcd(char a[], int len) {
 	int i, j = 0 ;
 	int r = 0, d ;
@@ -49,9 +53,9 @@ int bcd_toa( int d, char a[], int len ) {
 	return l ;
 }
 
-int bcd_add(int d1, int d2) {
+int bcd_add_aux(int d1, int d2, int carry) {
 	int d = 0, t ;
-	int i, carry = 0  ;
+	int i  ;
 	for (i=0; i<2*sizeof(int); i++) {
 	
 		t = (0xf&d1)+(0xf&d2)+carry  ;
@@ -68,6 +72,26 @@ int bcd_add(int d1, int d2) {
 	return d ;
 }
 
+int bcd_add(int d1, int d2) {
+	return bcd_add_aux(d1, d2, 0 ) ;
+}
+
+int bcd_9complement(int d) {
+	int d1 = 0, t ;
+	int i  ;
+	for (i=0; i<2*sizeof(int); i++) {
+		t = 9-(0xf&d) ;
+		d1 += t<<(4*i) ;
+		d >>= 4 ;
+	}
+	printf("(%d): d=%d\n", __LINE__, d1 ) ;
+	return d1 ;
+}
+
+int bcd_sub(int d1, int d2) {
+	int d = bcd_9complement(d2) ;
+	return bcd_add_aux(d1,d,1) ;
+}
 
 int main(int argc, char * argv[]) {
 	int d1, d2, d ;
@@ -76,6 +100,7 @@ int main(int argc, char * argv[]) {
 	char s[2*sizeof(int)+1] ;
 	int len ;
 	assert(argc==3) ;
+	char op = '+' ;
 
 	d1 = ato_bcd(argv[1], strlen(argv[1])) ;
 	d2 = ato_bcd(argv[2], strlen(argv[2])) ;
@@ -84,12 +109,17 @@ int main(int argc, char * argv[]) {
 	s1[len] = '\0' ;
 	len = bcd_toa(d2, s2, 2*sizeof(int)) ;
 	s2[len] = '\0' ;
-	
+
+#ifndef IS_SUB
 	d = bcd_add(d1, d2) ;
+#else
+	d = bcd_sub(d1, d2) ;
+	op = '-' ;
+#endif
+
 	len = bcd_toa(d, s, 2*sizeof(int)) ;
 	s[len] = '\0' ;
-	
-	printf("%s + %s = %s\n", s1, s2, s ) ;
+	printf("%s %c %s = %s\n", s1, op, s2, s ) ;
 	
 	
  	return 0 ;
