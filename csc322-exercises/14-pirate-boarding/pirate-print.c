143fe25be95a140fa07bff3b622fbeccce53e1d2
burtr <burt@cs.miami.edu>
2023-09-18 04:26:58 +0000

    ok

--- csc322-exercises/14-pirate-boarding/pirate-print.c
+++ csc322-exercises/14-pirate-boarding/pirate-print.c
@@ -1,29 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char ** location ;
 
+/*
+ * long john von neumann hooks the stack
+ * author: bjr
+ * last-update:
+ *    18 sep 2023
+ *
+ * NOTICE - THESE ARE PIRATE TECHINQUES! This code is most likely not portable
+ * across operating systems or machine architectures.
+ *
+ */
+
+
+// pirates also like globals
+
+char ** location ;
+int there_be_pirates = 0 ;
+
+// you need to find the pirate secret!
+//
 #define PIRATE_SECRET 44 
 
-void pirate_print(void) {
-	int marker ;
-	char * ip = (char *) &marker ;
-	//printf("%p %p\n", location, ip) ;
-	//*location = "There be pirates!" ;
-        *(ip+PIRATE_SECRET) = "There be pirates!" ;	
+void pirate_boarding(void) {
+	int stack_marker ;
+	char * ip = (char *) &stack_marker ;
+        *(ip+PIRATE_SECRET) = "*** There be pirates! ***" ;	
 	return ;
 }
 
-
-void print_it(char * to_print) {
-	location = &to_print ;	
-	pirate_print() ;
-	printf("%s\n", to_print) ;
+void print_it(char * string_pointer_on_stack) {
+	location = &string_pointer_on_stack ;	
+	if (there_be_pirates) {
+		pirate_boarding() ;
+	}
+	printf("%s\n", string_pointer_on_stack) ;
 	return ;
 }
-
 
 int main(int argc, char * argv[]) {
-	print_it("Are there pirates?") ;
+	printf("Are there pirates in these cyberwaters?\n") ;
+	print_it("No, there are no pirates.\n" ) ;
+	there_be_pirates = 1 ;
+	printf("Are there pirates in these cyberwaters?\n") ;
+	print_it("No, there are no pirates.\n" ) ;
 	return 0 ;
 }
+
